66ea696172ccdc0310c4de58f58fed6655387f78
tcolinbright <117130462+tcolinbright@users.noreply.github.com>
2023-02-20 12:23:14 -0800

    learning funtionality
    bssid tracker will scan for bssid every n seconds and return a crude scrolling bar graph to the display to help track down pesky AP's.

--- PicoW/Tests/list_manipulation.py
+++ PicoW/Tests/list_manipulation.py
@@ -8,7 +8,7 @@ import time
 nic = network.WLAN(network.STA_IF)
 nic.active(True)
 
-scan = nic.scan()
+
 
 
 def scan_for(bssid_in):
@@ -20,11 +20,13 @@ def scan_for(bssid_in):
         
         if bssid == bssid_in:
             dbm_recording.append(dbm)
-            print(f'SSID: {ssid}\n')
-            print(f'BSSID: {bssid}\n')
-            print(f'Dbm {dbm}\n')
-            print("\n*************************\n\n")
+            #print(f'SSID: {ssid}\n')
+            #print(f'BSSID: {bssid}\n')
+            #print(f'Dbm {dbm}\n')
+            #print("\n*************************\n\n")
         else:
+            #print(f'Could not locate: {bssid}\n')
+            #dbm_recording.append(0)
             pass
 
 
@@ -33,12 +35,42 @@ def trim_list(list_name, limit):
         list_name.pop(0)
         return list_name
 
+
+def get_avg(in_list):
+    average = sum(in_list)/len(in_list)
+    return average
+
+
+def simple_barchart(in_list):
+    for dbm in in_list:
+        pos_dbm = abs(dbm)
+        bar_length = pos_dbm
+        bar = f'  {dbm} dbm |' + ("-"  * bar_length)
+        print(bar)
+
+
+def other_barchart(in_list):
+    dbm = in_list[-1]
+    pos_dbm = abs(dbm)
+    bar_length = pos_dbm
+    bar = f'  {dbm} dbm |' + ("-"  * bar_length) + "*"
+    print(bar)
+
+
 dbm_recording = []
 
 
 while True:
-    trim_list(dbm_recording, 10)
-    print(dbm_recording)
+    scan = nic.scan()
+    trim_list(dbm_recording, 3)
+    
+
+    #dbm_avg = get_avg(dbm_recording)
+    #scans_used = len(dbm_recording)
+    #print(f'Avg: {dbm_avg} dbm')
+    #print(f'Avg of {scans_used} previous scans.\n')
+    #simple_barchart(dbm_recording)
     scan_for("00:71:C2:6F:5A:80")
+    other_barchart(dbm_recording)
     time.sleep(2)
 
