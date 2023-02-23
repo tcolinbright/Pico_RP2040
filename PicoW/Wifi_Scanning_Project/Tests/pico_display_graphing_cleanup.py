9c711747f6e649672d10bc8dc188e877cc1bcafe
tcolinbright <117130462+tcolinbright@users.noreply.github.com>
2023-02-22 21:01:53 -0800

    working on that graphing to display
    working on that graphing to display

--- PicoW/Wifi_Scanning_Project/Tests/pico_display_graphing_cleanup.py
+++ PicoW/Wifi_Scanning_Project/Tests/pico_display_graphing_cleanup.py
@@ -1,17 +1,17 @@
-'''Change to log dbm strengths from pico w'''
+# This example takes the temperature from the Pico's onboard temperature sensor, and displays it on Pico Display Pack, along with a little pixelly graph.
+# It's based on the thermometer example in the "Getting Started with MicroPython on the Raspberry Pi Pico" book, which is a great read if you're a beginner!
+
 import network
 import binascii
 import time
 from pimoroni import RGBLED
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2
 
 # set up the hardware
-display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, rotate=0)
+display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, rotate=0)
+led = RGBLED(6, 7, 8) # Pins 6, 7, 8 control R, G, B
 nic = network.WLAN(network.STA_IF)
 nic.active(True)
-
-led = RGBLED(6, 7, 8)
-
 # set the display backlight to 50%
 display.set_backlight(0.5)
 
@@ -20,35 +20,12 @@ WIDTH, HEIGHT = display.get_bounds()
 
 BLACK = display.create_pen(0, 0, 0)
 WHITE = display.create_pen(255, 255, 255)
+GREEN = display.create_pen(0, 255, 0)
 
 
-dbm_min = -90
-dbm_max = -10
 bar_width = 5
 
-dbms = [] # Empty list of -dbm readings
-
-colors = [(0, 0, 255), (0, 255, 0), (255, 255, 0), (255, 0, 0)]
-
-
-def dbm_to_color(dbm):
-    dbm = min(dbm, dbm_max)
-    dbm = max(dbm, dbm_min)
-
-    f_index = float(dbm - dbm_min) / float(dbm_max - dbm_min)
-    f_index *= len(colors) - 1
-    index = int(f_index)
-
-    if index == len(colors) - 1:
-        return colors[index]
-
-    blend_b = f_index - index
-    blend_a = 1.0 - blend_b
-
-    a = colors[index]
-    b = colors[index + 1]
-
-    return [int((a[i] * blend_a) + (b[i] * blend_b)) for i in range(3)]
+dbms = []
 
 
 def dbm_to_list(scan, bssid_in):
@@ -63,28 +40,31 @@ def dbm_to_list(scan, bssid_in):
             dbms.append(dbm)
         else:
             print(f'{bssid_in} not located...')
-        
-
 
 scan_this_one = ""
 
 while True:
     scan = nic.scan()
     dbm_to_list(scan, scan_this_one)
-
+    
     # fills the screen with black
     display.set_pen(BLACK)
     display.clear()
 
+    # the following two lines do some maths to convert the number from the temp sensor into celsius
+    #Append to list of dbms here
 
+    #temperatures.append(temperature)
+
+    # shifts the temperatures history to the left by one sample
     if len(dbms) > WIDTH // bar_width:
         dbms.pop(0)
 
     i = 0
 
     for dbm in dbms:
-        # chooses a pen colour based on the dbm
-        DBM_COLOUR = display.create_pen(*dbm_to_color(dbm))
+        # chooses a pen colour based on the temperature
+        DBM_COLOUR = BLACK
         display.set_pen(DBM_COLOUR)
 
         # draws the reading as a tall, thin rectangle
@@ -100,8 +80,8 @@ while True:
     display.rectangle(1, 1, 100, 25)
 
     # writes the reading as text in the white rectangle
-    #display.set_pen(BLACK)
-    #display.text("{:.2f}".format(dbm) + "dbm", 3, 3, 0, 3)
+    display.set_pen(BLACK)
+    #display.text("{:.2f}".format(dbm) + "c", 3, 3, 0, 3)
 
     # time to update the display
     display.update()
