40afc94577037e713fb4d583a64681ea0f77d364
tcolinbright <117130462+tcolinbright@users.noreply.github.com>
2023-04-21 08:06:56 -0700

    Update workingMQTT.py

--- PicoW/workingMQTT.py
+++ PicoW/workingMQTT.py
@@ -7,23 +7,23 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('connecting to network...\n')
-    wlan.connect("Astronomy Tower", "LemonDrop62442")
+    wlan.connect("SSID", "Password")
     while not wlan.isconnected():
          pass
 
 print(wlan.isconnected())
 
 
-mqtt_server = "67478721149d4950abb591176395df1a.s2.eu.hivemq.cloud"
+mqtt_server = "mqtt_server"
 port = 8883
 client_id = 'picofromshowerdht22'
-username = 'colinb'
-password = 'Q20111374'
+username = 'mqtt_username'
+password = 'mqtt_password'
 topic_pub = 'test'
 topic_msg = 'Testmessage'
 
 def mqtt_connect():
-    client = MQTTClient(client_id, mqtt_server, port, user=username, password=password, keepalive=3600, ssl=True, ssl_params={'server_hostname':'67478721149d4950abb591176395df1a.s2.eu.hivemq.cloud'})
+    client = MQTTClient(client_id, mqtt_server, port, user=username, password=password, keepalive=3600, ssl=True, ssl_params={'server_hostname':'servername'})
     client.connect()
     print('Connected to %s MQTT Broker'%(mqtt_server))
     return client
